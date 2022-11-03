56c5f1d8d9a7fda9821815f44543de30d76c6b50
Carlo Munguia <cmunguia@denimgroup.com>
2022-11-02 23:11:15 -0500

    cloning in rust

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 fn main() {
+    // let x = vec!["carlo".to_string()];
+    // let y = x;
+    // let z = y;
+    // println!("{:?}", z);
+
     let x = vec!["carlo".to_string()];
-    let y = x;
-    let z = y;
+    let y = x.clone();
+    let z = y.clone();
+    println!("{:?}", x);
+    println!("{:?}", y);
     println!("{:?}", z);
 }
