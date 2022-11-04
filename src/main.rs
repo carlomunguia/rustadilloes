0a4b2d95ecaf4c76ae8e6b2d8e5856085d6aea2b
Carlo Munguia <cmunguia@denimgroup.com>
2022-11-03 20:47:18 -0500

    more on ownership

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,36 @@ fn main() {
     // let z = y;
     // println!("{:?}", z);
 
-    let x = vec!["carlo".to_string()];
-    let y = x.clone();
-    let z = y.clone();
-    println!("{:?}", x);
-    println!("{:?}", y);
-    println!("{:?}", z);
+    //clone
+    // let x = vec!["carlo".to_string()];
+    // let y = x.clone();
+    // let z = y.clone();
+    // println!("{:?}", x);
+    // println!("{:?}", y);
+    // println!("{:?}", z);
+
+    // // copy
+    // let x = 1;
+    // let y = x;
+    // println!("x = {}, y = {}", x, y);
+
+    let s = String::from("takes");
+    takes_ownership(s);
+
+    let val = 1;
+    make_copy(val);
+}
+
+fn takes_ownership(s: String) {
+    let string = s;
+    println!("{}", string)
+}
+
+fn make_copy(one: i32) {
+    let val1 = one;
+    println!("{}", val1);
+}
+
+fn take_and_give(str2: String) -> String {
+    str2
 }
