df85208dec1cabec2359325c14687038491d023e
Carlo Munguia <cmunguia@denimgroup.com>
2022-11-04 22:33:29 -0500

    even more fun with slices

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,28 @@
 fn main() {
-    println!("Hello, Carlo!");
+    let my_string = String::from("Greetings human");
+
+    let word = first_word(&my_string[0..6]);
+    println!("The first word is: {}", word);
+    let word = first_word(&my_string[..6]);
+
+    println!("The first word is: {}", word);
+
+    let word = first_word(&my_string);
+    println!("The first word is: {}", word);
+
+    let my_string_literal = "Greetings human";
+
+    let word = first_word(&my_string_literal[0..6]);
+    println!("The first word is: {}", word);
+    let word = first_word(&my_string_literal[..6]);
+    println!("The first word is: {}", word);
+
+    let word = first_word(&my_string_literal);
+
+    println!("The first word is: {}", word);
 }
 
-fn first_word(s: &String) -> &str {
+fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
