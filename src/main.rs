f66af52447d1a3bebc5709372fc215238cc2d5c0
Carlo Munguia <cmunguia@denimgroup.com>
2022-11-04 22:27:39 -0500

    working with slices

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 fn main() {
-    let s1 = String::from("hikuri");
-
-    let len = calculate_length(&s1);
-
-    println!("The length of '{}' is {}.", s1, len);
+    println!("Hello, Carlo!");
 }
 
-fn calculate_length(s: &String) -> usize {
-    s.len()
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
