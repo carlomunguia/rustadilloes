c1ab47b6ab4f4fe6b8283be7135f8bd9028a5cfd
Carlo Munguia <cmunguia@denimgroup.com>
2022-11-07 19:58:12 -0600

    version 1 struct program

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,13 @@
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64,
-}
-
 fn main() {
-    let user1 = User {
-        email: String::from("carlo@example.com"),
-        username: String::from("carlo"),
-        active: true,
-        sign_in_count: 1,
-    };
+    let width1 = 30;
+    let height1 = 50;
 
-    println!("User 1: {}", user1.email);
-    println!("active: {}", user1.active);
-    println!("sign_in_count: {}", user1.sign_in_count);
-    println!("email: {}", user1.email);
-    println!("username: {}", user1.username);
-
-    let user2 = User {
-        email: String::from("texas@pecos.com"),
-        ..user1
-    };
-
-    println!("User 2: {}", user2.email);
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(width1, height1)
+    );
 }
 
-fn build_user(email: String, username: String) -> User {
-    User {
-        email,
-        username,
-        active: true,
-        sign_in_count: 1,
-    }
+fn area(width: u32, height: u32) -> u32 {
+    width * height
 }
