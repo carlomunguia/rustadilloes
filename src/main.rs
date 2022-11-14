8f014295e0f9e0a6062e3457dc376e0acae4ee0a
Carlo Munguia <cmunguia@denimgroup.com>
2022-11-13 23:58:05 -0600

    storing vals in hashmaps

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
-enum SpreadsheetCell {
-    Int(i32),
-    Float(f64),
-    Text(String),
-}
+use std::collections::HashMap;
+fn main() {
+    let mut scores = HashMap::new();
 
-let row = vec! [
-    SpreadsheetCell::Int(3),
-    SpreadsheetCell::Text(String::from("blue")),
-    SpreadsheetCell::Float(10.12),
-];
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name).copied().unwrap_or(0);
+
+    println!("{}: {}", team_name, score);
+}
