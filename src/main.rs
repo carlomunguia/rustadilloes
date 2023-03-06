e34e56b5882742616fe4789db1b1fb4abbf411b2
Carlo Munguia <cmunguia@denimgroup.com>
2023-03-05 23:20:50 -0600

    finishing up hashmaps & ownership of values inserted into hashmaps

--- src/main.rs
+++ src/main.rs
@@ -9,4 +9,34 @@ fn main() {
     let score = scores.get(&team_name).copied().unwrap_or(0);
 
     println!("{}: {}", team_name, score);
+
+    for (key, value) in &scores {
+        println!("{}: {}", key, value);
+    }
+
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+
+    map.insert(field_name, field_value);
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Blue"), 25);
+
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+
+    println!("{:?}", scores);
+
+    let text = "hello world wonderful world";
+
+    let mut mappy = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = mappy.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", mappy);
 }
